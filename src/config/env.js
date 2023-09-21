62b64e3813f2add96eb67f1a866fef6f0cc26937
VeraLaureano <laureanoivangerardo@gmail.com>
2023-09-21 10:07:28 -0300

    31/09/23 - login añadido

--- src/config/env.js
+++ src/config/env.js
@@ -6,11 +6,13 @@ const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 const VERSION = process.env.VERSION;
 const NODE_ENV = process.env.NODE_ENV;
+const SECRET_KEY = process.env.SECRET_KEY;
 
 // exports vars
 module.exports = {
   PORT,
   MONGO_URI,
   VERSION,
-  NODE_ENV
+  NODE_ENV,
+  SECRET_KEY
 };
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,35 +1,63 @@
-const { createUser } = require('../services/user.service');
+const jwt = require('jsonwebtoken');
 const escapeSpecialCharacters = require('../utils/escapeSpecialCharacters');
-const { encryptPassword } = require('../utils/bcrypt');
 const validatePassword = require('../utils/validatePassword');
+const { createUser, findOneUser } = require('../services/user.service');
+const { encryptPassword, comparePassword } = require('../utils/bcrypt');
+const { SECRET_KEY } = require('../config/env');
 
 const postUserSignup = async (req, res) => {
-  const { username, email, password, confirmPassword } = req.body;
+  try {
+    const { username, email, password, confirmPassword } = req.body;
 
-  if (password !== confirmPassword)
-    return res.status(400).json({message: 'PASSWORD_NOT_MATCH'});  
-
-  const escapedUsername = escapeSpecialCharacters(username);
-  const escapedEmail = escapeSpecialCharacters(email);
-  const escapedPassword = escapeSpecialCharacters(password);
-  const isPasswordValid = validatePassword(escapedPassword);
+    if (password !== confirmPassword)
+      return res.status(400).json({message: 'PASSWORD_NOT_MATCH'});  
   
-  if (!isPasswordValid)
-    return res.status(400).json({message: 'PASSWORD_DOES_NOT_PASS_THE_VALIDATION'});
+    const escapedUsername = escapeSpecialCharacters(username);
+    const escapedEmail = escapeSpecialCharacters(email);
+    const escapedPassword = escapeSpecialCharacters(password);
+    const isPasswordValid = validatePassword(escapedPassword);
+    
+    if (!isPasswordValid)
+      return res.status(400).json({message: 'PASSWORD_DOES_NOT_PASS_THE_VALIDATION'});
+  
+    const passwordHashed = encryptPassword(escapedPassword);
+  
+    const userData = {
+      username: escapedUsername,
+      email: escapedEmail,
+      password: passwordHashed
+    };
+  
+    await createUser(userData);
+  
+    return res.status(201).json({ signup: true });
+  } catch (error) {
+    console.error(error);  
+  }
+};
 
-  const passwordHashed = encryptPassword(escapedPassword);
+const postUserLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-  const userData = {
-    username: escapedUsername,
-    email: escapedEmail,
-    password: passwordHashed
-  };
+    const user = await findOneUser(email);
+    if (!user)
+      return res.status(401).json({message: 'INVALID_CREDENTIALS'});
 
-  await createUser(userData);
+    const isMatch = comparePassword(password, user.password);
 
-  return res.status(201).json({ signup: true });
+    if (!isMatch)
+      return res.status(401).json({message: 'INVALID_CREDENTIALS'});
+    
+    const token = jwt.sign({userId: user._id}, SECRET_KEY, {expiresIn: '1h'});
+
+    res.status(200).json({token});
+  } catch (error) {
+    console.error(error);  
+  }
 };
 
 module.exports = {
-  postUserSignup
+  postUserSignup,
+  postUserLogin
 };
